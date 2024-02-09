79ed7cea4dce9e9c32b1ee2e18424705be3fa56f
lscantillo <luissebastiancantillo@gmail.com>
2024-02-08 19:18:33 -0500

    ADD: fix turbo load

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "flowbite/dist/flowbite.turbo.js";
+
+// Fixed turbo load issue
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
